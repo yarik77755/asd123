f05280c0b1bae546ca8bce2a57e06cc5b0cdbd3c
yarik77755 <yardhd@gmail.com>
2020-05-25 10:55:36 +0300

    added lab3

---
+++ lab3.cpp
@@ -0,0 +1,27 @@
+//Средний уровень
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count=0;
+	int max = 0;
+	char const N = 10;
+	char arr[] = {1,2,'l',4,'y','u',9,9,0,'s'};
+	for (int i = 0; i < N; i++) {
+		if (arr[i] >=0&&arr[i]<=9 ) {
+			count++;
+			if (max < count) {
+				max = count;
+			}
+		}
+		else {
+			count = 0;
+
+		}
+	}
+	cout<<"max digital sequence length is "<<max<<endl;
+	return max;
+}
